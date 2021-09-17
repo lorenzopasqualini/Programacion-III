1028fa3a436baaab2d4ddd257faae268e523faad
Tobias Faur <tobiasfaur@MacBook-Air-de-Tobias.local>
2021-09-17 12:54:12 -0300

    agregar movies

--- prog3/src/components/Comp.js
+++ prog3/src/components/Comp.js
@@ -10,7 +10,8 @@ export default class Comp extends Component{
             movies: [],
             filterMovies: [],
             orientacion: 'movieComponentVertical',
-            mensaje: 'Horizontal Display'
+            mensaje: 'Horizontal Display',
+            page: 2
         }
     }
 
@@ -21,7 +22,7 @@ export default class Comp extends Component{
                 console.log(data.results);
                 this.setState({
                     movies: data.results,
-                    filterMovies: data.results
+                    filterMovies: data.results,
                 })
             })
             .catch(e => console.log(e));
@@ -62,12 +63,31 @@ export default class Comp extends Component{
             })
         }
     }
-
+    
+    
+    addCards(){
+        fetch(`http://api.themoviedb.org/3/movie/popular?api_key=0e3afdb0602092f1f215f06c2057dbc0&page=${this.state.page}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let arrayPrevio = this.state.movies;
+            let arrayActualizado = arrayPrevio.concat(data.results);
+            let paginaActualizada = this.state.page + 1;
+            
+            this.setState({
+                movies: arrayActualizado,
+                filterMovies: arrayActualizado,
+                page: paginaActualizada
+            })
+        })
+    }
+    
     render(){
         return (
             <div className={this.state.orientacion}>
                 <button onClick={()=> this.view()}> {this.state.mensaje} </button>
                 <Filtro filterFunc={(name)=> this.filterFunc(name)} />
+                <button onClick={()=>this.addCards()}>Agregar mas</button>
                 {this.state.movies === [] ?
                     <h4> Cargando </h4>:
                     this.state.filterMovies.map((props, index) => {
@@ -77,4 +97,7 @@ export default class Comp extends Component{
             </div>
         )
     }
+       
 }
+    
+
